d053f7aa594408e297eecae22cd25ad180faa536
giovanni-arduini <gionnibigudd@gmail.com>
2024-11-18 15:40:47 +0100

    ADD array objects

--- app.js
+++ app.js
@@ -9,7 +9,31 @@ posts = [
     title: "Ciambellone",
     content: "La ricetta del ciambellone",
     immainge: "imgs/ciambellone.jpeg",
-    tags: ["farina", "burro", "estratto vaniglia"],
+    tags: ["#classico", "#leggero", "#veloce"],
+  },
+  {
+    title: "Cracker di barbabietola",
+    content: "La ricetta del cracker di barbabietola",
+    immainge: "imgs/cracker_barbabietola.jpeg",
+    tags: ["#sfizioso", "#colorato", "#aperitivo"],
+  },
+  {
+    title: "Pane fritto dolce",
+    content: "La ricetta del pane fritto dolce",
+    immainge: "imgs/pane_fritto_dolce.jpeg",
+    tags: ["#goloso", "#salato", "#aperitivo"],
+  },
+  {
+    title: "Pasta di barbabietola",
+    content: "La ricetta della pasta di barbabietola",
+    immainge: "imgs/pasta_brarbabietola.jpeg",
+    tags: ["#colorato", "#primipiatti", "#leggero"],
+  },
+  {
+    title: "Torta paesana",
+    content: "La ricetta della torta paesana",
+    immainge: "imgs/torta_paesana.jpeg",
+    tags: ["#tradizionale", "#autunnale", "#goloso"],
   },
 ];
 
